7c0c1b23383c1b5020417d79fe6aaff789dab6f9
ShawnPence <45135345+ShawnPence@users.noreply.github.com>
2019-12-06 00:08:09 -0600

    day 6 refactor to remove debugging visualization

--- AdventOfCode2019/AdventOfCode2019/Day6.cs
+++ AdventOfCode2019/AdventOfCode2019/Day6.cs
@@ -51,58 +51,23 @@ namespace AdventOfCode2019
 		static int uncommonAncestorCount(Dictionary<string, string> orbits, string start, string start2) 
 		{
 			HashSet<string> o1 = new HashSet<string>();
-			List<string> path1 = new List<string>();
-			//path1.Add(start);
 			start = orbits[start];
-			path1.Add(start);
 			while (orbits[start] != "COM")
 			{
 				o1.Add(start);
 				start = orbits[start];
-				path1.Add(start);
 			}
 			HashSet<string> o2 = new HashSet<string>();
-			List<string> path2 = new List<string>();
-			//path2.Add(start2);
 			start2 = orbits[start2]; //start at the object "YOU" are orbiting
-			path2.Add(start2);
 			while (orbits[start2] != "COM")
 			{
 				o2.Add(start2);
 				start2 = orbits[start2];
-				path2.Add(start2);
 			}
 			HashSet<string> all = new HashSet<string>();
 			all.UnionWith(o1);
 			all.UnionWith(o2);
 			int commoncount = (o2.Count + o1.Count) - all.Count;
-			int path1Count = path1.Count;
-			int path2Count = path2.Count;
-
-			//visualize - used for debugging to find my mistake on initial attempt
-			StringBuilder p1 = new StringBuilder(path1.Count * 4);
-			foreach (string s in path1) p1.Append(s + "-");
-			StringBuilder p2 = new StringBuilder(path2.Count * 4);
-			foreach (string s in path2) p2.Append(s + "-");
-
-			using (StreamWriter sw = new StreamWriter("puzzlepaths.txt"))
-			{
-				sw.WriteLine(p1.ToString());
-				sw.WriteLine("");
-				sw.WriteLine(p2.ToString());
-			}
-
-			List<string> commonP = new List<string>();
-
-			while (path1[path1.Count - 1] == path2[path2.Count - 1])
-			{
-				commonP.Add(path1[path1.Count - 1]);
-				path1.RemoveAt(path1.Count - 1);
-				path2.RemoveAt(path2.Count - 1);
-				
-			}
-			Console.WriteLine("paths count:" + (path1.Count + path2.Count).ToString());
-
 			int result = (o2.Count + o1.Count - 2 * commoncount);
 
 			return result;
