b37ef0bc264490bddc485405b17578e4f41b9160
ShawnPence <45135345+ShawnPence@users.noreply.github.com>
2019-12-14 00:45:41 -0600

    refactor day 14

--- AdventOfCode2019/AdventOfCode2019/Day14.cs
+++ AdventOfCode2019/AdventOfCode2019/Day14.cs
@@ -23,31 +23,31 @@ namespace AdventOfCode2019
 			{
 				while (sr.Peek() != -1) lines.Add(sr.ReadLine());
 			}
-			var d = new Dictionary<string, Dictionary<string, long>>();//compound / inputs&quantity to create
-			var d2 = new Dictionary<string, long>();//compund / output quantity from inputs in d
+			var inputs = new Dictionary<string, Dictionary<string, long>>();//item / inputs&quantity to create
+			var outputs = new Dictionary<string, long>();//item / output quantity from items in inputs[item]
 			foreach(var line in lines)
 			{
-				var ing = new Dictionary<string, long>();
+				var inputParts = new Dictionary<string, long>();
 				var sides = line.Split("=>");
 
 				var output = sides[1].Trim().Split(" ")[1];
-				long q = Convert.ToInt64(sides[1].Trim().Split(" ")[0]);
+				long quantity = Convert.ToInt64(sides[1].Trim().Split(" ")[0]);
 
 				var parts = sides[0].Split(",");
 				foreach (var part in parts)
 				{
 					var partName = part.Trim().Split(" ")[1];
 					var partAmt = Convert.ToInt64(part.Trim().Split(" ")[0]);
-					ing[partName] = partAmt;
+					inputParts[partName] = partAmt;
 				}
-				d[output] = ing;
-				d2[output] = q;
+				inputs[output] = inputParts;
+				outputs[output] = quantity;
 
 			}
 
 			var need = new Dictionary<string, long>();
 			need["FUEL"] = 1;
-			long needed = GetAmount(d, d2, need);
+			long needed = GetAmount(inputs, outputs, need);
 			Console.WriteLine(needed); //part 1
 
 			long max = 2000000000000 / needed;
@@ -55,16 +55,16 @@ namespace AdventOfCode2019
 			
 			while (max > min)
 			{
-				long target = (min + max) / 2;
+				long testValue = (min + max) / 2;
 				var needTest = new Dictionary<string, long>();
-				needTest["FUEL"] = target;
-				if (GetAmount(d, d2, needTest) < 1000000000000)
+				needTest["FUEL"] = testValue;
+				if (GetAmount(inputs, outputs, needTest) < 1000000000000)
 				{
-					min = target + 1;
+					min = testValue + 1;
 				}
 				else
 				{
-					max = target;
+					max = testValue;
 				}
 			
 			}
@@ -72,57 +72,56 @@ namespace AdventOfCode2019
 
 		}
 
-		static long MaxDepth(Dictionary<string, Dictionary<string, long>> d, string key)
+		static long MaxDepth(Dictionary<string, Dictionary<string, long>> inputs, string key)
 		{
 			if (key == "ORE") return 0;
 			long md = -1;
-			foreach (var k in d[key].Keys)
+			foreach (var k in inputs[key].Keys)
 			{
-				md = Math.Max(md, 1 + MaxDepth(d, k));
+				md = Math.Max(md, 1 + MaxDepth(inputs, k));
 			}
 			return md;
 		}
 
-		static long GetAmount(Dictionary<string, Dictionary<string, long>> d, Dictionary<string,long>d2, Dictionary<string,long> need)
+		static long GetAmount(Dictionary<string, Dictionary<string, long>> inputs, Dictionary<string,long>outputs, Dictionary<string,long> need)
 		{
 			while (need.Keys.Count > 1 || need.ContainsKey("FUEL"))
 			{
-				var n2 = new Dictionary<string, long>();
-				long md = -1;
+				var needNew = new Dictionary<string, long>();
+				long maxDepth = -1;
 				foreach (var k in need.Keys)
 				{
-					md = Math.Max(md, MaxDepth(d, k));
+					maxDepth = Math.Max(maxDepth, MaxDepth(inputs, k));
 				}
 
 				foreach (var k in need.Keys)
 				{
 					if (k == "ORE")
 					{
-						n2[k] = need[k];
+						needNew[k] = need[k];
 					}
 					else
 					{
 						//only convert the parts that are furthest from ore to their component parts (to avoid creating more of a component than necessary)
-						if (MaxDepth(d,k) == md)
+						if (MaxDepth(inputs,k) == maxDepth)
 						{
 
-							long minMultiple = need[k] % d2[k] == 0 ? need[k] / d2[k] : need[k] / d2[k] + 1;
-							if (need[k] < d2[k]) minMultiple = 1;
-							foreach (var kin in d[k].Keys)
+							long minMultiple = need[k] % outputs[k] == 0 ? need[k] / outputs[k] : need[k] / outputs[k] + 1;
+							foreach (var kInput in inputs[k].Keys)
 							{
-								if (!n2.ContainsKey(kin)) n2[kin] = 0;
-								n2[kin] += minMultiple * d[k][kin];
+								if (!needNew.ContainsKey(kInput)) needNew[kInput] = 0;
+								needNew[kInput] += minMultiple * inputs[k][kInput];
 							}
 						}
 						else
 						{
-							if (!n2.ContainsKey(k)) n2[k] = 0;
-							n2[k] += need[k];
+							if (!needNew.ContainsKey(k)) needNew[k] = 0;
+							needNew[k] += need[k];
 						}
 
 					}
 				}
-				need = n2;
+				need = needNew;
 			}
 			return need["ORE"];
 		}
