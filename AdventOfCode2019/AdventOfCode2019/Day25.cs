b26d3cae7b67a0dfd203236161c0fb5a623a15c1
ShawnPence <45135345+ShawnPence@users.noreply.github.com>
2019-12-25 00:09:52 -0600

    Day 25

---
+++ AdventOfCode2019/AdventOfCode2019/Day25.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AdventOfCode2019
+{
+	class Day25
+	{
+		public static void Problems()
+		{
+			Console.WriteLine("Input file:");
+			string fileName = Console.ReadLine();
+			while (!File.Exists(fileName))
+			{
+				Console.WriteLine("file not found - try again:");
+				fileName = Console.ReadLine();
+			}
+			var c = new Computer(fileName);
+			bool quit = false;
+			while (!quit)
+			{
+				c.Run();
+				while (c.Output.Count > 0)
+				{
+					char x = (char)c.Output.Dequeue();
+
+					if (x == 10)
+					{
+						Console.Write("\n");
+					}
+					else
+					{
+						Console.Write(x);
+					}
+				}
+				string input = Console.ReadLine();
+				if (input == "RESET")
+				{
+					c = new Computer(fileName);
+				}
+				else if (input == "QUIT")
+				{
+					quit = true;
+				}
+				else
+				{
+					foreach (char x in input)
+						c.Input(x);
+					c.Input(10);
+				}
+
+			}
+
+
+		}
+	}
+}
--- AdventOfCode2019/AdventOfCode2019/Program.cs
+++ AdventOfCode2019/AdventOfCode2019/Program.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode2019
 	{
 		static void Main()
 		{
-			Day24.Problems();
+			Day25.Problems();
 		}
 	}
 }
